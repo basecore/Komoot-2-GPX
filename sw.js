e313bc658b19fa8654a862ccaf53fc415c461eb1
basecore <roesser.sebastian@gmail.com>
2026-04-29 06:37:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
-// Minimaler Service Worker für PWA-Installierbarkeit
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', function(e) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+    // Dieser leere Fetch-Befehl ist zwingend nötig, 
+    // damit Chrome den "App Installieren" Button anzeigt!
+});
